181d92141755584d8f80b5ce58bb070eea149a98
Tushar Daiya <tushardaiya@outlook.com>
2025-03-02 14:04:55 +0530

    fixes

--- src/app/api/leaderboard/route.ts
+++ src/app/api/leaderboard/route.ts
@@ -1,6 +1,6 @@
 import { auth } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
-import { NextRequest, NextResponse } from "next/server";
+import {  NextResponse } from "next/server";
 
 export async function GET() {
   const session = await auth();
--- src/components/ui/app-sidebar.tsx
+++ src/components/ui/app-sidebar.tsx
@@ -15,68 +15,6 @@ export async function AppSidebar() {
     take: 5,
   });
   console.log(searchResults);
-  const fakeSearchQueries = [
-    {
-      id: "1",
-      createdAt: new Date("2024-03-02T10:00:00Z"),
-      userId: "user_123",
-      queryText: "how to become a millionaire overnight",
-    },
-    {
-      id: "2",
-      createdAt: new Date("2024-03-02T10:05:00Z"),
-      userId: "user_123",
-      queryText: "is water wet",
-    },
-    {
-      id: "3",
-      createdAt: new Date("2024-03-02T10:10:00Z"),
-      userId: "user_456",
-      queryText: "why does my code not work",
-    },
-    {
-      id: "4",
-      createdAt: new Date("2024-03-02T10:15:00Z"),
-      userId: "user_789",
-      queryText: "free ways to get rich fast",
-    },
-    {
-      id: "5",
-      createdAt: new Date("2024-03-02T10:20:00Z"),
-      userId: "user_456",
-      queryText: "how to hack NASA from my fridge",
-    },
-    {
-      id: "6",
-      createdAt: new Date("2024-03-02T10:25:00Z"),
-      userId: "user_789",
-      queryText: "what happens if you microwave a spoon",
-    },
-    {
-      id: "7",
-      createdAt: new Date("2024-03-02T10:30:00Z"),
-      userId: "user_123",
-      queryText: "why do I hear boss music when coding",
-    },
-    {
-      id: "8",
-      createdAt: new Date("2024-03-02T10:35:00Z"),
-      userId: "user_456",
-      queryText: "best hiding spots if I don’t do my assignments",
-    },
-    {
-      id: "9",
-      createdAt: new Date("2024-03-02T10:40:00Z"),
-      userId: "user_789",
-      queryText: "can I sell my soul for WiFi speed",
-    },
-    {
-      id: "10",
-      createdAt: new Date("2024-03-02T10:45:00Z"),
-      userId: "user_123",
-      queryText: "does pineapple belong on pizza",
-    },
-  ];
 
   return (
     <Sidebar>
