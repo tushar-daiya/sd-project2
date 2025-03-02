e29bf2b383a7661b33864f6f58235d34f621fefc
Tushar Daiya <tushardaiya@outlook.com>
2025-03-02 14:06:07 +0530

    fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,12 @@
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+  if (session) {
+    redirect("/dashboard");
+  } else {
+    redirect("/login");
+  }
   return <div>hello</div>;
 }
